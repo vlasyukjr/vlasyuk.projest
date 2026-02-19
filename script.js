af180d980ceb7e7fa536a7a5ea571557bedf6981
vlasyukjr <bogdanvlasuk52@gmail.com>
2026-02-19 14:01:05 +0200

    Update script.js
    Signed-off-by: vlasyukjr <bogdanvlasuk52@gmail.com>

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function scrollToSection() {
+    document.getElementById("about").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+const faders = document.querySelectorAll('.fade');
+
+window.addEventListener('scroll', () => {
+    faders.forEach(el => {
+        const rect = el.getBoundingClientRect();
+        if (rect.top < window.innerHeight - 100) {
+            el.classList.add('show');
+        }
+    });
+});
 
